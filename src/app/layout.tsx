88715b378ae6f66a4eb4f9e132e5acb2346ddc48
Ainsley Woo <125954904+Kantosaurus@users.noreply.github.com>
2025-06-09 13:06:47 +0800

    readded favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,20 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "DiscoverSUTD",
   description: "Your gateway to exploring clubs, events, and opportunities at SUTD",
+  icons: {
+    icon: [
+      {
+        url: '/DSUTD logo clear.png',
+        type: 'image/png',
+      }
+    ],
+    apple: [
+      {
+        url: '/DSUTD logo clear.png',
+        type: 'image/png',
+      }
+    ],
+  },
 };
 
 export default function RootLayout({
